109279c60d92d81ab18e432a8b03437e5f0b4a60
Leoian0808 <12130383@me.mcu.edu.tw>
2025-04-22 16:40:52 +0800

    page 35

---
+++ Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; } = null!;
+        public Student Student { get; set; } = null!;
+    }
+}
